3612b5ec54f885630287b973c444080eb9d10671
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-06-26 10:26:09 +0900

    C#のコード追加。 とりあえずシリアライズオブジェクトインタフェースとメモリストリームインタフェース。

---
+++ CSharp/YanaPOnlineUtil/Stream/MemoryStream.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YanaPOnlineUtil.Stream
+{
+	/// <summary>
+	/// メモリストリームインタフェース
+	/// </summary>
+	public interface IMemoryStream
+	{
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref int Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref uint Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref short Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref ushort Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref char Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref byte Data);
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Data">データ</param>
+		/// <returns>成功したらtrueを返す</returns>
+		bool Serialize(ref string Data);
+	}
+}
