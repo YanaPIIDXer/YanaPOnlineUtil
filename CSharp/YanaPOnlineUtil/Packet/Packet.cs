4e671394154c29ed2586efa4d3261b25d12fdb03
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-06-28 13:03:25 +0900

    パケットクラス追加。

---
+++ CSharp/YanaPOnlineUtil/Packet/Packet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YanaPOnlineUtil.Stream;
+
+namespace YanaPOnlineUtil.Packet
+{
+	/// <summary>
+	/// パケットクラス
+	/// </summary>
+	public abstract class Packet : ISerializable
+	{
+		/// <summary>
+		/// パケットＩＤ
+		/// </summary>
+		public abstract byte PacketId { get; }
+
+		/// <summary>
+		/// シリアライズ
+		/// </summary>
+		/// <param name="Stream">ストリーム</param>
+		/// <returns>成功したらtrueを返す</returns>
+		public abstract bool Serialize(IMemoryStream Stream);
+	}
+}
